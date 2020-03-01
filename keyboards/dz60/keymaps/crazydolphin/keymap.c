fc48b666c2a54253ae171ed9109165c51a592941
vimiomori <vivian.muchen@gmail.com>
2020-03-01 16:14:56 +0900

    idk

--- keyboards/dz60/keymaps/crazydolphin/keymap.c
+++ keyboards/dz60/keymaps/crazydolphin/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,      KC_ENT,
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, MO(_FN),
-	    TO(_VIM),      KC_LALT,      KC_LGUI,                      KC_SPC,                    MO(_LIGHTS),           KC_RGUI,         MO(_MOUSE)),
+	    TO(_VIM), KC_LALT,      KC_LGUI,                      KC_SPC,                    MO(_LIGHTS),           KC_RGUI,         MO(_MOUSE)),
 
 
 /* FN LAYER
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, ______, ______, ______, ______, ______, ______,
 		RGB_MOD,          RGB_HUI, RGB_SAI, RGB_VAI,  ______,   ______,  ______,  ______,  ______, ______, ______, ______, ______, ______,
 		______,           RGB_HUD, RGB_SAD, RGB_VAD,  ______,   ______,  ______,  ______,  ______, ______, ______, ______, ______,
-		______,           ______,  ______,  ______,   ______,  ______,  ______,  ______,  ______, ______, ______,         ______, ______,
+		______,           ______,  ______,  ______,   ______,  ______,  ______,  ______,  ______, ______, ______,          ______, ______,
 		______,  ______,           ______,                      ______,                          ______,   ______,         ______),
 
 
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_MOUSE] = LAYOUT_60_tsangan_hhkb(
 		______, KC_ACL0, KC_ACL1, KC_ACL2, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-		______,          ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_MS_UP, ______, ______,  ______,
-		______,             ______,  ______,  ______,  ______,  ______,  ______,  ______,   ______,  KC_MS_LEFT,   KC_MS_DOWN,  KC_MS_RIGHT,   ______,
-		______,            ______,  ______,  ______, ______, ______,   ______,  ______,  ______,  KC_BTN1, KC_BTN2,     ______,  ______,
-		______,         ______,           ______,                    ______,                              ______,    ______,        ______),
+		______,         ______, ______, ______, ______, ______, ______, ______, ______,  ______,  KC_MS_U, ______, ______,  ______,
+		______,         ______, ______, ______, ______, ______, ______, ______, ______,  KC_MS_L, KC_MS_D, KC_MS_R,   ______,
+		______,         ______, ______, ______, ______, ______, ______, ______, ______,  KC_BTN1, KC_BTN2,       ______,  ______,
+		______, ______,           ______,                    ______,                          ______,    ______,        ______),
 
 
  /* VIM LAYER
@@ -120,10 +120,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 	[_VIM] = LAYOUT_60_tsangan_hhkb(
-		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-		______,          ______, ______, ______,  ______,   ______,  ______,  ______,  TO(_DEFAULT), ______, ______, ______, ______, ______,
-		______,           ______, ______, ______,  ______,   ______,  KC_RIGHT,  KC_DOWN,  KC_UP, KC_LEFT, ______, ______, ______,
-		______,           ______,  ______,  ______,   ______,  ______,  ______,  ______,  ______, ______, ______,         ______, ______,
+		______, ______, ______, ______, ______, ______, ______, ______,   ______,  ______, ______, ______, ______, ______, ______,
+		______,         ______, ______, ______, ______, ______, ______,   ______,  TO(_DEFAULT), ______, ______, ______, ______, ______,
+		______,         ______, ______, ______, ______, ______, KC_RIGHT, KC_DOWN, KC_UP, KC_LEFT, ______, ______, ______,
+		______,         ______, ______, ______, ______, ______, ______,   ______,  ______, ______, ______,         ______, ______,
 		______,  ______,           ______,                      ______,                          ______,   ______,         ______),
 
 };
@@ -143,13 +143,52 @@ combo_t key_combos[COMBO_COUNT] = {
 
 void process_combo_event(uint8_t combo_index, bool pressed) {
     switch(combo_index) {
-        case WRITE:
-            if (pressed) {
-                register_code(KC_LGUI);
-                register_code(KC_S);
-                unregister_code(KC_LGUI);
-                unregister_code(KC_S);
-            }
-            break;
+    case WRITE:
+        if (pressed) {
+            register_code(KC_LGUI);
+            register_code(KC_S);
+            unregister_code(KC_LGUI);
+            unregister_code(KC_S);
+        }
+        break;
     }
 };
+
+
+uint32_t layer_state_set_user(uint32_t state) {
+    switch (biton32(state)) {
+    case _FN:
+        rgblight_setrgb (0x00,  0x00, 0xFF);
+        break;
+    case _MOUSE:
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    case _VIM:
+        rgblight_setrgb (0x00,  0xFF, 0x00);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_setrgb (0x00,  0xFF, 0xFF);
+        break;
+    }
+  return state;
+};
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case FOO:
+//       if (record->event.pressed) {
+//         // Do something when pressed
+//       } else {
+//         // Do something else when release
+//       }
+//       return false; // Skip all further processing of this key
+//     case KC_ENTER:
+//       // Play a tone when enter is pressed
+//       if (record->event.pressed) {
+//         PLAY_NOTE_ARRAY(tone_qwerty);
+//       }
+//       return true; // Let QMK send the enter press/release events
+//     default:
+//       return true; // Process all other keycodes normally
+//   }
+// }
